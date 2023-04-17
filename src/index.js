d52ad9bba62e4990a7392cd75256c058a1b9257c
Everett Butler <everettjamesbutler@gmail.com>
2023-04-17 17:13:39 -0500

    got basic index.js set up with one reducer for testing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+
+const wellness = (state = 0, action) => {
+    if (action.type === 'SET_WELLNESS_SCORE') {
+        return action.payload;
+    } else if (action.type === 'CLEAR FORM') {
+        return 0;
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers(
+        {
+            wellness,
+        }
+    ),
+    applyMiddleware(logger)
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
