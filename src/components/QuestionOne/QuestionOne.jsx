814aa78e3c86edc15d4a7d0ddf4c4922921e4123
Everett Butler <everettjamesbutler@gmail.com>
2023-04-17 17:08:31 -0500

    got the first question basic jsx file set up. beginnging inner functions now

---
+++ src/components/QuestionOne/QuestionOne.jsx
@@ -0,0 +1,17 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+function QuestionOne() {
+
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default QuestionOne;
