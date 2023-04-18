bdb45951b702838a402503e34330f277b7e9cc84
Everett Butler <everettjamesbutler@gmail.com>
2023-04-18 16:43:59 -0500

    got to a stopping point. could not figure out the delete function. but got the admin to display the data!

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-function Admin() {
+function Admin({ entry }) {
     const dispatch = useDispatch();
     const history = useHistory();
     const feedbackList = useSelector(store => store.feedbackList);
@@ -21,17 +21,34 @@ function Admin() {
         });
     }
 
+    const deleteEntry = (e) => {
+        console.log(`deleteEntry ${feedbackList.id}`)
+        axios.delete(`/feedback/${feedbackList.id}`)
+        .then((response) => {
+            fetchFeedbackList();
+        }).catch((error) => {
+            console.log(`Error in deleteEntry ${error}`);
+            alert('Something went wrong!')
+        });
+    }
+
+    const returnHome = (event) => {
+        history.push('/');
+    }
 
     return(
         <>
-            <h3>ADMIN PAGE: SEE RESULTS OF ALL SURVEYS</h3><hr />
+            <h3>ADMIN PAGE: SEE RESULTS OF ALL SURVEYS</h3>
+            <button onClick={returnHome}>Home</button>
+            <hr />
             {
-                feedbackList.map(response => (
-                    <div key={response.id}>
-                        <p>Wellness: {response.wellness}</p>
-                        <p>Goofyness: {response.goofyness}</p>
-                        <p>Support: {response.suppport}</p>
-                        <p>Comments: {response.comments}</p>
+                feedbackList.map(entry => (
+                    <div key={entry.id}>
+                        <p>Wellness: {entry.wellness}</p>
+                        <p>Goofyness: {entry.goofyness}</p>
+                        <p>Support: {entry.suppport}</p>
+                        <p>Comments: {entry.comments}</p>
+                        <button onClick={deleteEntry}>Delete Entry</button>
                         <hr />
                     </div>
                 ))
