60c7c6b4f7932d8d6024dc52dfed0af8bbe9e02c
Everett Butler <everettjamesbutler@gmail.com>
2023-04-18 16:00:17 -0500

    got a delete request working!

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -30,4 +30,16 @@ router.post('/', (req, res) => {
         });
 });
 
+//DELETE ENTRY
+router.delete('/:id', (req, res) => {
+    console.log('In DELETE request');
+    let queryText = `DELETE FROM 'feedback' WHERE 'id' = $1`;
+    pool.query(queryText, [req.params.id]).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    })
+});
+
 module.exports = router;
